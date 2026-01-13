86aedaab902795038655551de4042b530da75196
carl <yashino538@gmail.com>
2026-01-13 22:11:00 +0800

    feat: optimize GA injection for Atlas and Zenith sites

--- examples/zenith-site/src/client/app.tsx
+++ examples/zenith-site/src/client/app.tsx
@@ -1,6 +1,14 @@
 import { createInertiaApp } from '@inertiajs/react'
+import { useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
+
+declare global {
+  interface Window {
+    dataLayer: any[]
+    gtag: (...args: any[]) => void
+  }
+}
 
 createInertiaApp({
   resolve: (name) => {
@@ -8,6 +16,29 @@ createInertiaApp({
     return pages[`./pages/${name}.tsx`]
   },
   setup({ el, App, props }) {
-    createRoot(el).render(<App {...props} />)
+    const GAApp = (props: any) => {
+      const gaId = props.initialPage.props.ga_id
+
+      useEffect(() => {
+        if (gaId && typeof window !== 'undefined') {
+          // Initialize GA
+          const script = document.createElement('script')
+          script.async = true
+          script.src = `https://www.googletagmanager.com/gtag/js?id=${gaId}`
+          document.head.appendChild(script)
+
+          window.dataLayer = window.dataLayer || []
+          window.gtag = function gtag() {
+            window.dataLayer.push(arguments)
+          }
+          window.gtag('js', new Date())
+          window.gtag('config', gaId)
+        }
+      }, [gaId])
+
+      return <App {...props} />
+    }
+
+    createRoot(el).render(<GAApp {...props} />)
   },
 })
